5d56ec558e7733f1081f97369e405ef321081c4e
nhatthm <nt@hellofresh.com>
2021-04-12 15:05:16 +0200

    Add Buffer.Reset()

--- buffer.go
+++ buffer.go
@@ -14,20 +14,30 @@ type Buffer struct {
 // Write appends the contents of p to the buffer, growing the buffer as
 // needed. The return value n is the length of p; err is always nil. If the
 // buffer becomes too large, Write will panic with ErrTooLarge.
-func (s *Buffer) Write(p []byte) (n int, err error) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (b *Buffer) Write(p []byte) (n int, err error) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
 
-	return s.buffer.Write(p)
+	return b.buffer.Write(p)
 }
 
 // String returns the contents of the unread portion of the buffer
 // as a string. If the Buffer is a nil pointer, it returns "<nil>".
 //
 // To build strings more efficiently, see the strings.Builder type.
-func (s *Buffer) String() string {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (b *Buffer) String() string {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
 
-	return s.buffer.String()
+	return b.buffer.String()
+}
+
+// Reset resets the buffer to be empty,
+// but it retains the underlying storage for use by future writes.
+// Reset is the same as Truncate(0).
+func (b *Buffer) Reset() {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	b.buffer.Reset()
 }
---
+++ buffer_test.go
@@ -0,0 +1,22 @@
+package surveymock
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBuffer_Reset(t *testing.T) {
+	t.Parallel()
+
+	buf := new(Buffer)
+
+	_, err := buf.Write([]byte("hello world"))
+	require.NoError(t, err)
+
+	assert.Equal(t, "hello world", buf.String())
+
+	buf.Reset()
+	assert.Empty(t, buf.String())
+}
